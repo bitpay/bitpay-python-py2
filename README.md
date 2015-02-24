2592865e5d43fb48849df4952d2cec28330cacff
Paul Daigle <paul@bitpay.com>
2015-02-24 13:50:14 -0500

    create setup for pypi

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# bitpay-python27
-Python 2.7 Library for the BitPay API
+# BitPay Library for Python 
+Powerful, flexible, lightweight interface to the BitPay Bitcoin Payment Gateway API.
+
+[![Supported Python versions](https://pypip.in/py_versions/bitpay-py2/badge.svg)](https://pypi.python.org/pypi/bitpay/)
+[![PyPi Package](https://pypip.in/version/bitpay-py2/badge.svg)](https://pypi.python.org/pypi/bitpay/2.2.0)
+[![](https://travis-ci.org/bitpay/bitpay-python-py2.svg?branch=master)](https://travis-ci.org/bitpay/bitpay-python-py2)
+
+This library is only compatible with Python 2. Please use the bitpay-python library for Python installations.
+
+## [Getting Started &raquo;](http://dev.bitpay.com/guides/python.html)
+
+## API Documentation
+
+API Documentation is available on the [BitPay site](https://bitpay.com/api).
+
+## Running the Tests
+
+Before running the behavior tests, you will need a test.bitpay.com account and you will need to set the local constants. 
+
+To set constants:
+    > source tasks/set_constants.sh "https://test.bitpay.com" your@email yourpassword
+
+To run unit tests:
+    > nosetests
+
+To run behavior tests:
+    > behave
+    
+## Found a bug?
+Let us know! Send a pull request or a patch. Questions? Ask! We're here to help. We will respond to all filed issues.
---
+++ setup.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+from distutils.core import setup
+setup(
+    name = "bitpay-py2",
+    packages = ["bitpay"],
+    version = "2.2.0",
+    description = "Accept bitcoin with BitPay",
+    author = "Paul Daigle",
+    author_email = "integrations@bitpay.com",
+    url = "https://github.com/bitpay/bitpay-python",
+    download_url = "https://github.com/bitpay/bitpay-python/tarball/v2.2.0",
+    keywords = ["bitcoin", "payments", "crypto"],
+    license = "MIT License",
+    classifiers = [
+"Programming Language :: Python",
+"Programming Language :: Python :: 2.7",
+"Programming Language :: Python :: 2 :: Only",
+"Development Status :: 5 - Production/Stable",
+"Environment :: Web Environment",
+"Intended Audience :: Developers",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+"Topic :: Software Development :: Libraries :: Python Modules",
+"Topic :: Office/Business :: Financial"
+],
+    long_description = """\
+Python Library for integrating with BitPay
+-------------------------------------
+
+This library is a simple way to integrate your application with
+BitPay for taking bitcoin payments. It exposes three basic 
+functions, authenticating with bitpay, creating invoices, 
+and retrieving invoices. It is not meant as a replacement for 
+the entire BitPay API. However, the key_utils module contains
+all of the tools you need to use the BitPay API for other
+purposes.
+
+© 2015 BitPay, Inc.
+
+This version requires Python 3 or later; a Python 2 version is available separately.
+"""
+)
